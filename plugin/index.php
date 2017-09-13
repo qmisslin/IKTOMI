013ea83e8847418d9e53bda8820082c49d04c563
qmisslin <misslin.quentin@gmail.com>
2017-09-14 00:16:07 +0200

    Modification graphique de l'affichage du plugin par défaut (plugin texte simple)

--- plugin/index.php
+++ plugin/index.php
@@ -3,21 +3,34 @@
 	<head>
 	<meta charset="UTF-8">
 	<style>
-		@keyframes turn {
-		    from { transform: rotate(0deg);}
-		    to {transform: rotate(360deg);}
-		 }
-		#Roue_Arriere{
-			animation: turn 5s linear 0s infinite;
+		body{
+			background-color: #34495e;
+			text-align: center;
+			margin: auto 5vw auto 5vw;
 		}
-		#Roue_Avant{
-			animation: turn 5s linear 0s infinite;
-		}
+		h1{
+			text-shadow:
+				0 1px 0px #bdc3c7,
+				0 -1px 0px #bdc3c7,
+				1px 0 0px #bdc3c7,
+				-1px 0 0px #bdc3c7,
 
+				1px 1px 0px #bdc3c7,
+				2px 2px 0px #bdc3c7,
+				3px 3px 0px #bdc3c7,
+				4px 4px 0px #bdc3c7;
+			font-size: 10vh;
+			color:white;
+		}
+		p{
+			font-size: 6vh;
+			color: #212f3d;
+
+		}
 	</style>
 	<body>
 		
 		<h1>Bonjour, voici un plugin php pour Iktomi</h1>
-		<p>POur le moment, ce plugin affiche simplement du texte, pour pouvoir tester les réléchargement de plugins dans l'application Iktomi</p>
+		<p>Pour le moment, ce plugin affiche simplement du texte, pour pouvoir tester les téléchargement de plugins dans l'application Iktomi</p>
 	</body>
 </html>
